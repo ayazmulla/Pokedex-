04293acee46d522a6156828fa5c406a91ee3e1f6
ayazmulla <ayazmulla2004@gmail.com>
2026-03-12 15:53:28 +0530

    details.tsx

--- details.tsx
+++ details.tsx
@@ -1,28 +1,78 @@
-import { Stack } from "expo-router";
-import { useLocalSearchParams } from "expo-router/build/hooks";
-import { useEffect } from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { Image, ScrollView, StyleSheet, Text } from "react-native";
 
 export default function Details() {
   const params = useLocalSearchParams();
-  console.log(params.name);
 
-  useEffect(() => {}, []);
+  const [pokemon, setPokemon] = useState<any>(null);
 
-  async function fetchPokemonByName(name: string) {
-    // try{} catch()
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  async function fetchPokemon() {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`);
+
+    const data = await res.json();
+
+    setPokemon(data);
   }
+
+  if (!pokemon) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <>
-      <Stack.Screen options={{ title: params.name as string }} />
-      <ScrollView
-        contentContainerStyle={{
-          gap: 16,
-          padding: 16,
-        }}
-      ></ScrollView>
+      <Stack.Screen options={{ title: pokemon.name }} />
+
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image
+          source={{ uri: pokemon.sprites.front_default }}
+          style={styles.image}
+        />
+
+        <Text style={styles.name}>{pokemon.name.toUpperCase()}</Text>
+
+        <Text style={styles.text}>Height: {pokemon.height}</Text>
+
+        <Text style={styles.text}>Weight: {pokemon.weight}</Text>
+
+        <Text style={styles.text}>
+          Type: {pokemon.types.map((t: any) => t.type.name).join(", ")}
+        </Text>
+
+        <Text style={styles.text}>
+          Abilities:{" "}
+          {pokemon.abilities.map((a: any) => a.ability.name).join(", ")}
+        </Text>
+      </ScrollView>
     </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    padding: 20,
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+  },
+
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+
+  text: {
+    fontSize: 18,
+    marginTop: 10,
+  },
+});
+
+
